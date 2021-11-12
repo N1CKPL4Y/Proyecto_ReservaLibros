b48531aa578caa6f6a3bcf2dbd2e6294257b2903
N1CKPL4Y <nicolasperez3e2017@gmail.com>
2021-11-12 00:20:19 -0300

    creacion tablas categoria, libro, registro_reserva

--- app/src/main/java/com/example/proyecto_reservalibros/DataBase/AdminSQLiteOpenHelper.java
+++ app/src/main/java/com/example/proyecto_reservalibros/DataBase/AdminSQLiteOpenHelper.java
@@ -8,10 +8,14 @@ import androidx.annotation.Nullable;
 public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
 
     private SQLiteDatabase db;
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NOMBRE = "biblioapp";
 
-    public AdminSQLiteOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public AdminSQLiteOpenHelper(@Nullable Context context) {
+        super(context, DATABASE_NOMBRE, null, DATABASE_VERSION);
     }
+
+
 
     //donde creo mis tablas
     @Override
@@ -31,8 +35,25 @@ public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
         db.execSQL("INSERT INTO Usuario VALUES(null, 'Nicolas', 'Perez', 'nico123', 2);");
         db.execSQL("INSERT INTO Usuario VALUES(null, 'Jose', 'Pino', 'jose123', 2);");
         //tabla categoria_libro
-        db.execSQL("");
-
+        db.execSQL("CREATE TABLE categoria_libro(id integer primary key autoincrement," +
+                "nombre_c varchar(50));");
+        //insert categorias
+        db.execSQL("INSERT INTO Categoria_libro VALUES(null, 'suspenso')");
+        db.execSQL("INSERT INTO Categoria_libro VALUES(null, 'accion')");
+        db.execSQL("INSERT INTO Categoria_libro VALUES(null, 'romance')");
+        //tabla libro
+        db.execSQL("CREATE TABLE Libro(id integer primary key autoincrement, nombre varchar(50)," +
+                "cantidad_paginas integer, foreign key(categoria_id_fk) references Categoria(id));");
+        //insert libro
+        db.execSQL("INSERT INTO Libro VALUES(null, 'IT(eso)', 400, 1)");
+        db.execSQL("INSERT INTO Libro VALUES(null, 'El prisionero de zenda', 300, 2)");
+        db.execSQL("INSERT INTO Libro VALUES(null, 'Francisca yo te amo', 200, 3)");
+        //tabla historial de reservas
+        db.execSQL("CREATE TABLE Registro_Reserva(id integer primary key autoincrement," +
+                "fecha_hora TEXT," +
+                "foreign key(usuario_id_fk) REFERENCES Usuario(id)," +
+                "foreign key(libro_id_fk) REFERENCES Libro(id)," +
+                "foreign key(categoria_id_fk) REFERENCES Categoria(id));");
 
     }
     //donde actualizo mi BD
