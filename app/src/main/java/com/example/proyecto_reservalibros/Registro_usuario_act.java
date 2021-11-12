dc243720e8391ba4ff3385f6ce084495061e8a17
N1CKPL4Y <nicolasperez3e2017@gmail.com>
2021-11-12 00:24:38 -0300

    creacion activity registro_usuario, ver registro de reservas de admin

---
+++ app/src/main/java/com/example/proyecto_reservalibros/Registro_usuario_act.java
@@ -0,0 +1,14 @@
+package com.example.proyecto_reservalibros;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registro_usuario_act extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_usuario);
+    }
+}
---
+++ app/src/main/java/com/example/proyecto_reservalibros/Ver_registro_reservas_admin_act.java
@@ -0,0 +1,14 @@
+package com.example.proyecto_reservalibros;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ver_registro_reservas_admin_act extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ver_registro_reservas_admin);
+    }
+}
