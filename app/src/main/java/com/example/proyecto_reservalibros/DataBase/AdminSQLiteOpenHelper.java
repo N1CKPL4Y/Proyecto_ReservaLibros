d9b2e939358ab059ba1819ac864ca206bd5b3302
N1CKPL4Y <nicolasperez3e2017@gmail.com>
2021-11-11 22:52:29 -0300

    creacion tablas tipo_usuario, usuario

--- app/src/main/java/com/example/proyecto_reservalibros/DataBase/AdminSQLiteOpenHelper.java
+++ app/src/main/java/com/example/proyecto_reservalibros/DataBase/AdminSQLiteOpenHelper.java
@@ -7,6 +7,7 @@ import androidx.annotation.Nullable;
 
 public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
 
+    private SQLiteDatabase db;
 
     public AdminSQLiteOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
@@ -15,6 +16,24 @@ public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
     //donde creo mis tablas
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        //tabla tipo_usuario
+        db.execSQL("CREATE TABLE Tipo_usuario(id integer primary key," +
+                "nombre_t varchar(50));");
+        //insert tipo de usuarios
+        db.execSQL("INSERT INTO Tipo_usuario VALUES(1, 'Administrador')");
+        db.execSQL("INSERT INTO Tipo_usuario VALUES(2, 'Cliente')");
+        //tabla usuario
+        db.execSQL("CREATE TABLE Usuario(id integer primary key autoincrement," +
+                "nombre varchar(50), apellido varchar(50), passwd varchar(50)," +
+                "foreign key(tipo_usuario_fk) references Tipo_usuario(id));");
+        //insert usuarios
+        db.execSQL("INSERT INTO Usuario VALUES(null, 'admin', 'admin', 'admin123', 1)");
+        db.execSQL("INSERT INTO Usuario VALUES(null, 'Nicolas', 'Perez', 'nico123', 2);");
+        db.execSQL("INSERT INTO Usuario VALUES(null, 'Jose', 'Pino', 'jose123', 2);");
+        //tabla categoria_libro
+        db.execSQL("");
+
+
     }
     //donde actualizo mi BD
     @Override
