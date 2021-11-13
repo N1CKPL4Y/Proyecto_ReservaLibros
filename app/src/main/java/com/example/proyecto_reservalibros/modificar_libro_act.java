db91cef65968e63132ffd4440740071ef3347360
JOSE <josepfuente22@gmail.com>
2021-11-12 22:00:22 -0300

    ayer: creacion activity menu principal
    hoy 12 nov:
    
    - agregue 2 logos, 1 a cada activity
    - creacion de activity registro de usuario
    - creacion de activity administrador
    //break
    
    - creacion de activity:
    
    (falta logo para las activity)
    
    añadir libros
    modificar libros
    añadir categoria
    ver registros de reservas

---
+++ app/src/main/java/com/example/proyecto_reservalibros/modificar_libro_act.java
@@ -0,0 +1,14 @@
+package com.example.proyecto_reservalibros;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class modificar_libro_act extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_modificar_libro);
+    }
+}
---
+++ app/src/main/java/com/example/proyecto_reservalibros/registro_reservasAdmi_act.java
@@ -0,0 +1,14 @@
+package com.example.proyecto_reservalibros;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class registro_reservasAdmi_act extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_reservas_admi);
+    }
+}
